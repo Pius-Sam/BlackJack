2e6b1defeeacdd2bf5a7af1d47cd8217fcc9ad54
Pius-Sam <61335378+Pius-Sam@users.noreply.github.com>
2020-02-22 00:47:00 +0100

    ...

---
+++ App.py
@@ -0,0 +1,14 @@
+from LinkedList import LinkedList
+
+
+linkedlist = LinkedList()
+
+linkedlist.insert_start(12)
+linkedlist.insert_start(13)
+linkedlist.insert_start(14)
+linkedlist.insert_start(15)
+
+linkedlist.traverse_list()
+linkedlist.remove(12)
+linkedlist.traverse_list()
+
